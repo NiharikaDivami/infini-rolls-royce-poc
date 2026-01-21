62e14b8fa3e648b087a47c2abe46a40d1e507334
giresh 18 <giresh@divami.com>
2026-01-21 18:45:01 +0530

    intial commit

--- src/components/BorderButton/BorderButton.tsx
+++ src/components/BorderButton/BorderButton.tsx
@@ -14,6 +14,9 @@ export default function BorderButton({ children, variant = 'draw meet', classNam
   return (
     <button className={`${styles.button} ${variantClasses} ${className}`}>
       {children}
+      <svg viewBox="0 0 276 46" xmlns="http://www.w3.org/2000/svg">
+        <path d="M 0.5 23 A 22.5 22.5 0 0 1 23 0.5 L 253 0.5 A 22.5 22.5 0 0 1 275.5 23 A 22.5 22.5 0 0 1 253 45.5 L 23 45.5 A 22.5 22.5 0 0 1 0.5 23" />
+      </svg>
     </button>
   );
 }
